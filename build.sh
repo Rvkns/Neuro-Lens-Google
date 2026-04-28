bcdeb64f9a977b4b23f4f9eaf7c9ad5a3b549843
Valerio <Sentz01@gmail.com>
2026-04-28 22:46:35 +0200

    Add dummy .env to satisfy flutter build assets check

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Script per installare Flutter e compilare il progetto su Vercel
 
+echo "Creazione file .env fittizio per soddisfare pubspec.yaml..."
+touch .env
+
 echo "Scaricando Flutter..."
 git clone https://github.com/flutter/flutter.git -b stable
 export PATH="$PATH:`pwd`/flutter/bin"
